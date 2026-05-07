b554bc9aaf77cec88ec6f8f5c67bc2d244bf830c
Johnny.Wang <johnny.wang@btse.com>
2026-05-07 14:43:20 +0800

    sql: LC 1384. Total Sales Amount by Year done

--- sql_training/total_sales_amount_by_year/answer.sql
+++ sql_training/total_sales_amount_by_year/answer.sql
@@ -1,1 +1,27 @@
--- 1.
+-- 1. Recursive CTE
+with recursive cte as (select product_id,
+                              period_start,
+                              period_end,
+                              average_daily_sales,
+                              extract(year from period_start)::int as report_year
+                       from sales
+
+                       union all
+
+                       select product_id,
+                              period_start,
+                              period_end,
+                              average_daily_sales,
+                              report_year + 1
+                       from cte
+                       where report_year < extract(year from period_end)::int)
+select p.product_id,
+       p.product_name,
+       c.report_year,
+       (least(c.period_end, make_date(c.report_year, 12, 31)) -- end_date or end of the year
+            - greatest(c.period_start, make_date(c.report_year, 1, 1))-- start_date or start of the year
+           + 1) -- add one day to include both start and end date
+           * c.average_daily_sales as total_amount
+from cte c
+         join product p on c.product_id = p.product_id
+order by p.product_id;
